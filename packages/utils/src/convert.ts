72270788df089f892a304009e2c863ffe9a29e44
Comdex <wcomdex@foxmail.com>
2023-10-22 21:38:45 +0800

    add Uint8ArrayToField

--- packages/utils/src/convert.ts
+++ packages/utils/src/convert.ts
@@ -3,3 +3,13 @@ import { Field } from "o1js";
 export function fieldArrayToStringArray(fields: Field[]): string[] {
     return fields.map((field) => field.toString());
 }
+
+export function uint8ArrayToField(arr: Uint8Array) {
+  let bigIntValue = 0n;
+
+  for (let i = 0; i < arr.length; i++) {
+    bigIntValue = (bigIntValue << 8n) | BigInt(arr[i]);
+  }
+
+  return Field(bigIntValue);
+}
