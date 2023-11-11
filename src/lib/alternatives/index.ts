87677b260bfe60c5081ebd0c638e8266fb244191
coldstar1993 <coldstar1993@163.com>
2023-11-12 00:09:55 +0800

    adjust export

---
+++ src/lib/alternatives/index.ts
@@ -0,0 +1,11 @@
+export * from './interfaces/append_only_tree';
+export * from './interfaces/indexed_tree';
+export * from './interfaces/merkle_tree';
+export * from './interfaces/update_only_tree';
+export * from './sparse_tree/sparse_tree';
+export * from './standard_indexed_tree/standard_indexed_tree';
+export * from './standard_tree/standard_tree';
+export * from './types'
+export { INITIAL_LEAF } from './tree_base';
+export { newTree } from './new_tree';
+export { loadTree } from './load_tree';
