7245b0532224c1eae2d0db97d6b2b6ee1aa32a38
coldstar1993 <coldstar1993@163.com>
2023-11-13 23:28:44 +0800

    add greaterThanFor254BitField for field comparison

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { Bool, Field, Provable } from 'o1js';
+import { Bool, Field, Provable, provablePure } from 'o1js';
 
 export {
   createEmptyValue,
@@ -195,3 +195,54 @@ export const bufferToInt256 = (buf: Buffer) => {
 
   return res;
 };
+
+export function separateHighPartFor254BitField(x: Field): {
+  xDiv2Var: Field;
+  isOddVar: Field;
+} {
+  let { xDiv2Var, isOddVar } = Provable.witness(
+    provablePure({ xDiv2Var: Field, isOddVar: Field }),
+    () => {
+      let bits = x.toBits();
+      Provable.log('bitlen: ', bits.length);
+      let isOdd = bits.shift()!.toBoolean() ? 1n : 0n;
+
+      return {
+        xDiv2Var: Field.fromBits(bits),
+        isOddVar: Field(isOdd),
+      };
+    }
+  );
+
+  xDiv2Var.mul(2).add(isOddVar).assertEquals(x);
+  return {
+    xDiv2Var,
+    isOddVar,
+  };
+}
+
+// Negative numbers are not supported. only support 254 bit field
+export function greaterThanFor254BitField(x: Field, y: Field) {
+  let { xDiv2Var: xHighPart, isOddVar: xParity } =
+    separateHighPartFor254BitField(x);
+  let { xDiv2Var: yHighPart, isOddVar: yParity } =
+    separateHighPartFor254BitField(y);
+
+  Provable.log(
+    'greaterThanFor254BitField - xHighPart: ',
+    xHighPart,
+    ', xParity: ',
+    xParity
+  );
+  Provable.log(
+    'greaterThanFor254BitField - yHighPart: ',
+    yHighPart,
+    ', yParity: ',
+    yParity
+  );
+  let highPartGreaterThan = xHighPart.greaterThan(yHighPart);
+  let highPartEqual = xHighPart.equals(yHighPart);
+  let parityGreaterThan = xParity.greaterThan(yParity);
+
+  return highPartGreaterThan.or(highPartEqual.and(parityGreaterThan));
+}
